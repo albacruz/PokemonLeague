ec4e72dec6c75b4dba782070503523549db5e911
Alba Cruz Torres <albacruztorres@gmail.com>
2019-05-29 12:23:25 +0100

    Progress bar funciona

--- src/js/index.js
+++ src/js/index.js
@@ -11,14 +11,9 @@ const createBattle = () => {
   return battleArray;
 }
 
-const f1Attacks = () => {
-  pokemon1.innerHTML = `<h2 id="nombre1">${nameFighter1.toUpperCase()}</h2><img class="pokemonPic" src="${images[padZero(idFighter1)]}" alt="${nameFighter1}"><div id="progress"><div id="barF1">${lifeF1}</div></div><div class="botonAttack1"><button id="button">Attack</button></div>`;
-  pokemon2.innerHTML = `<h2 id="nombre2">${nameFighter2.toUpperCase()}</h2><img class="pokemonPic" src="${images[padZero(idFighter2)]}" alt="${nameFighter2}"><div id="progress"><div id="barF2">${lifeF2}</div></div><div class="botonAttack2"></div>`;
-}
-
-const f2Attacks = () => {
-  pokemon1.innerHTML = `<h2 id="nombre1">${nameFighter1.toUpperCase()}</h2><img class="pokemonPic" src="${images[padZero(idFighter1)]}" alt="${nameFighter1}"><div id="progress"><div id="barF1">${lifeF1}</div></div><div class="botonAttack1"></div>`;
-  pokemon2.innerHTML = `<h2 id="nombre2">${nameFighter2.toUpperCase()}</h2><img class="pokemonPic" src="${images[padZero(idFighter2)]}" alt="${nameFighter2}"><div id="progress"><div id="barF2">${lifeF2}</div></div><div class="botonAttack2"><button id="button">Attack</button></div>`;
+const fighterAttacks = () => {
+  document.querySelector('#button1').classList.toggle('hidden');
+  document.querySelector('#button2').classList.toggle('hidden');
 }
 
 const receiveAttack = () => {
@@ -30,27 +25,28 @@ const receiveAttack = () => {
     barF1.style.width = lifeF1;
     barF1.textContent = lifeF1;
     pos = 1;
-    f2Attacks();
+    fighterAttacks();
+    fight("button1");
   }
   else{
     lifeF2 = (parseInt(lifeF2) - battle[0].baseStats.attack) + '%';
     barF2.style.width = lifeF2;
     barF2.textContent = lifeF2;
     pos = 0;
-    f1Attacks();
+    fighterAttacks();
+    fight("button2");
   }
-
-  fight();
 }
 
-const fight = () => document.getElementById("button").addEventListener('click', receiveAttack);
+const fight = (btn) => document.getElementById(btn).addEventListener('click', receiveAttack);
 
 const padZero = (n) => n.toString().padStart(3, '0');
 
 const battleInit = () => {
   document.querySelector('.boton').classList.add('hidden');
-  f1Attacks();
-  fight();
+  pokemon1.innerHTML = `<h2 id="nombre1">${nameFighter1.toUpperCase()}</h2><img class="pokemonPic" src="${images[padZero(idFighter1)]}" alt="${nameFighter1}"><div id="progress"><div id="barF1">${lifeF1}</div></div><div class="botonAttack"><button id="button1">Attack</button></div>`;
+  pokemon2.innerHTML = `<h2 id="nombre2">${nameFighter2.toUpperCase()}</h2><img class="pokemonPic" src="${images[padZero(idFighter2)]}" alt="${nameFighter2}"><div id="progress"><div id="barF2">${lifeF2}</div></div><div class="botonAttack"><button class = "hidden" id="button2">Attack</button></div>`;
+  fight("button1");
 }
 
 let randomNumber1 = getRandomNumber();
@@ -59,11 +55,9 @@ let randomNumber2 = getRandomNumber();
 const lifeIni = 100 + '%';
 
 let lifeF1 = lifeIni;
-console.log(lifeF1);
 let lifeF2 = lifeIni;
-console.log(lifeF2);
 
-let pos = 0;
+let pos = 1;
 
 const battle = createBattle();
 
